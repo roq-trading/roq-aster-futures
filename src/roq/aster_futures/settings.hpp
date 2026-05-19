3ee46732710fbaa58f1ec7d3da24e29edbb84645
thraneh <thraneh@gmail.com>
2026-05-19 15:28:34 +0000

    move gateway settings to flags

--- src/roq/aster_futures/settings.hpp
+++ src/roq/aster_futures/settings.hpp
@@ -2,49 +2,12 @@
 
 #pragma once
 
-#include <fmt/format.h>
-
-#include "roq/server/flags/settings.hpp"
-
-#include "roq/aster_futures/flags/flags.hpp"
-#include "roq/aster_futures/flags/mbp.hpp"
-#include "roq/aster_futures/flags/misc.hpp"
-#include "roq/aster_futures/flags/rest.hpp"
-#include "roq/aster_futures/flags/ws.hpp"
+#include "roq/aster_futures/flags/settings.hpp"
 
 namespace roq {
 namespace aster_futures {
 
-struct Settings final : public server::flags::Settings, public flags::Flags {
-  explicit Settings(args::Parser const &);
-
-  flags::Misc misc;
-  flags::REST rest;
-  flags::WS ws;
-  flags::MBP mbp;
-};
+using Settings = flags::Settings;
 
 }  // namespace aster_futures
 }  // namespace roq
-
-template <>
-struct fmt::formatter<roq::aster_futures::Settings> {
-  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
-  auto format(roq::aster_futures::Settings const &value, format_context &context) const {
-    using namespace std::literals;
-    return fmt::format_to(
-        context.out(),
-        R"({{)"
-        R"(misc={}, )"
-        R"(rest={}, )"
-        R"(ws={}, )"
-        R"(mbp={}, )"
-        R"(server={})"
-        R"(}})"sv,
-        value.misc,
-        value.rest,
-        value.ws,
-        value.mbp,
-        static_cast<roq::server::Settings const &>(value));
-  }
-};
