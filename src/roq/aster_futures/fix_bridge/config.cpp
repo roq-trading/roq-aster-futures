c81b4d1a263da71c6eb7d2a70c3d7f4bfbecbb4a
thraneh <thraneh@gmail.com>
2026-06-28 02:21:02 +0000

    moving fix_bridge into roq-server

---
+++ src/roq/aster_futures/fix_bridge/config.cpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#include "roq/aster_futures/fix_bridge/config.hpp"
+
+using namespace std::literals;
+
+namespace roq {
+namespace aster_futures {
+namespace fix_bridge {
+
+// === IMPLEMENTATION ===
+
+}  // namespace fix_bridge
+}  // namespace aster_futures
+}  // namespace roq
---
+++ src/roq/aster_futures/fix_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/aster_futures/gateway/config.hpp"
+
+namespace roq {
+namespace aster_futures {
+namespace fix_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace fix_bridge
+}  // namespace aster_futures
+}  // namespace roq
