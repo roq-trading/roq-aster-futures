6e15a26e71b7ca0dcaa6b831cd8ba91377331e64
thraneh <thraneh@gmail.com>
2026-07-04 03:18:56 +0000

    preparing roq-*-proto-bridge

---
+++ src/roq/aster_futures/proto_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/aster_futures/gateway/config.hpp"
+
+namespace roq {
+namespace aster_futures {
+namespace proto_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace proto_bridge
+}  // namespace aster_futures
+}  // namespace roq
